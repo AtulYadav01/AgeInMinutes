4ebde36831dd8c47871033946326ca9f7ccdd935
Ashu1533 <ashuy033@gmail.com>
2020-09-03 15:30:32 +0530

    Added new Functionality

--- app/src/main/java/com/atulyadav/ageinminutes/MainActivity.kt
+++ app/src/main/java/com/atulyadav/ageinminutes/MainActivity.kt
@@ -1,11 +1,40 @@
 package com.atulyadav.ageinminutes
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.DatePickerDialog
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btndatepicker.setOnClickListener {view ->
+            clickDatePicker(view)
+           }
     }
+
+    fun clickDatePicker(view : View) {
+        val myCalendar = Calendar.getInstance()
+        val year = myCalendar.get(Calendar.YEAR)
+        val month = myCalendar.get(Calendar.MONTH)
+        val day = myCalendar.get(Calendar.DAY_OF_MONTH)
+        DatePickerDialog(this, {
+                view, Selectedyear, Selectedmonth, SelecteddayOfMonth ->
+            val SelectedDate = "$SelecteddayOfMonth/ ${Selectedmonth+1}/ $Selectedyear"
+
+            tvSelectedDate.setText(SelectedDate)
+
+            val sdf = SimpleDateFormat("dd/MM/yyyy")
+        }
+            ,year
+            ,month,
+            day).show()
+    }
+
 }
+
