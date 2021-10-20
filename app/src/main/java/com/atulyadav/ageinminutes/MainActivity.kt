c020dde100ae6fc1aee5c5aae45fb7c242ecaea3
AtulYadav01 <ashuy033@gmail.com>
2021-10-20 21:35:40 +0530

    Added New Function.

--- app/src/main/java/com/atulyadav/ageinminutes/MainActivity.kt
+++ app/src/main/java/com/atulyadav/ageinminutes/MainActivity.kt
@@ -15,6 +15,41 @@ class MainActivity : AppCompatActivity() {
         btndatepicker.setOnClickListener {
             clickDatePicker()
         }
+        private fun clickDatePicker() {
+            val myCalendar = Calendar.getInstance()
+            val year = myCalendar.get(Calendar.YEAR)
+            val month = myCalendar.get(Calendar.MONTH)
+            val day = myCalendar.get(Calendar.DAY_OF_MONTH)
+            val dpd = DatePickerDialog(
+                this, { _, Selectedyear, Selectedmonth, SelecteddayOfMonth ->
+                    val selectedDate = "$SelecteddayOfMonth/ ${Selectedmonth + 1}/ $Selectedyear"
+    
+                    tvSelectedDate.text = selectedDate
+    
+                    val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
+    
+                    val theDate = sdf.parse(selectedDate)
+    
+                    val selectedDateInMinutes = theDate!!.time / 60000
+    
+                    val currentDate = sdf.parse(sdf.format(System.currentTimeMillis()))
+                    val currentDateInMinutes = currentDate!!.time / 60000
+    
+                    val differenceInMinutes = currentDateInMinutes - selectedDateInMinutes
+    
+                    tvSelectedDateInMinute.text = differenceInMinutes.toString()
+    
+    
+                }, year, month,
+                day
+            )
+            dpd.datePicker.maxDate = Date().time - 86400000
+            dpd.show()
+        }
+    
+    }
+    
+    
     }
 
   
